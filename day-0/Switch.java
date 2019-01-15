73e3619b24b5a8c133b75e90e21070b94b690f88
Rajat Bhansali <rajatbhansali01@gmail.com>
2019-01-16 02:25:18 +0530

    Switch in java

---
+++ day-0/Switch.java
@@ -0,0 +1,15 @@
+class Switch{
+	public static void main(String args[]){
+		int num=2;
+		switch(num)
+		{
+			case 1:System.out.println("Rajat Bhansali is the  student of mca");
+				break;
+			case 2:System.out.println("user  is the best student of mca");
+			break;
+			case 3:System.out.println("looser  is the best student of mca");
+			break;
+			default:System.out.println("not found");
+		}
+	}
+}
