bb4034de619fe0d179c8196d3e7d955df609b42e
Rajat Bhansali <rajatbhansali01@gmail.com>
2019-01-16 02:24:17 +0530

    Else of java

---
+++ day-0/Else.java
@@ -0,0 +1,12 @@
+class Else{
+	public static void main(String args[]){
+		int a=19;
+		int b=89;
+		if(a==b)
+		{
+			System.out.println(" a and b is equal");
+	}
+	else
+		System.out.println("Not equal");
+	}
+}
