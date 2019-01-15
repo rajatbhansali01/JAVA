90055845bc266a091760672b0914d2acfd4b92b9
Rajat Bhansali <rajatbhansali01@gmail.com>
2019-01-16 02:23:24 +0530

    If Condition Program

---
+++ day-0/If.java
@@ -0,0 +1,15 @@
+class If{
+public static void main(String args[])
+{
+	int a=10;
+	int b=30;
+	if(a>=b)
+	{
+		System.out.println("a is gretor");
+	}
+	else if(a<=b)
+	{
+		System.out.println("a is smaller");
+	}
+}
+}
