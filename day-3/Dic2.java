f21351e6e9efe9ce7ec322d995c772d5f68fb2e5
Rajat Bhansali <rajatbhansali01@gmail.com>
2019-01-19 23:38:08 +0530

    Create Dic2.java

---
+++ day-3/Dic2.java
@@ -0,0 +1,13 @@
+class Dic1{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		if(a<=b)
+		{
+			System.out.println("a is greator");
+		}
+		else
+			System.out.println("B is greator");
+	}
+}
